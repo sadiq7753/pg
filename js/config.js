db2a3121328e393771d08d897fd2b5c79b037f93
sadiq ahmad <sadiq7753@gmail.com>
2015-08-03 17:23:35 +0500

    more services added

--- js/config.js
+++ js/config.js
@@ -3,7 +3,11 @@ var local = "";
 var URL = "http://" + online + "/parentsguide/service.svc/";
 var config = {
 	endpointsURL : {
-		login : URL + "checkLogin"
+		login : URL + "checkLogin",
+        InsertChild : URL + "InsertChild",
+        InsertService : URL + "InsertService",
+        InsertOrganisation : URL + "InsertOrganisation",
+        InsertInvitation : URL + "InsertInvitation"
         
 	}
 };
--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,45 @@ $(document).on('pageinit', '#pageone', function(){
     });
 });
 
+
+$(document).on('pageinit', '#pagethree', function(){ 
+	$(document).on('click', '#addchild', function() {
+		return addChild();
+    });
+});
+
+
+$(document).on('pageinit', '#pagefour', function(){ 
+	$(document).on('click', '#addservice', function() {
+		return addService();
+    });
+});
+
+
+$(document).on('pageinit', '#pagefive', function(){ 
+	$(document).on('click', '#addorg', function() {
+		return addOrg();
+    });
+});
+
+$(document).on('pageinit', '#pagesix', function(){ 
+	$(document).on('click', '#addinvite', function() {
+		return addInvite();
+    });
+});
+
+
 function login() {
-    if($('#username').val != '' && $('#password').val != '') {
+   
+    if($('#username').val().length > 0 && $('#password').val().length != '') {
+      //  alert(config.endpointsURL.login);
         $.ajax({
             url : config.endpointsURL.login,
             data : {
-                username : $('#username').val,
-                password : $('#password').val
+                username : $('#username').val(),
+                password : $('#password').val()
             },
-            type : 'post',
+            type : 'get',
             async : 'true',
             dataType : 'json',
             beforeSend: function() {
@@ -25,9 +55,12 @@ function login() {
                 $.mobile.loading('hide');
             },
             success : function(response) {
-                sessionStorage.setItem("userId", response.userId);
-                sessionStorage.setItem("user_full_name", response.userFullName);
-                isLoggedIn();
+                if(response.d == "0") {
+                    alert("Invalid usernmae or password");
+                } else {
+                    sessionStorage.setItem("insertBy", response.d);
+                    $.mobile.changePage("#pagethree", { transition: 'slide'});
+                }
             },
             error : function(request, error) {
                 alert(request.statusText + "\n" + request.responseText);
@@ -36,7 +69,162 @@ function login() {
     }
 }
 
-$(document).on('pagebeforeshow', '#pageone,#pagetwo,#pagethree,#pagefour,#pagefive,#pagesix,#pageseven,#pageeight', function(){
+
+function addChild() {  
+  //  if($('#name').val().length != '' && $('#age').val().length != 0) {
+        alert(config.endpointsURL.InsertChild);
+        $.ajax({
+            url : config.endpointsURL.InsertChild,
+            data : {
+                name : $('#name').val(),
+                dob : $('#dob').val(),
+                gender : $('#gender').val(),
+                remarks : $('#remarks').val(),
+                status: "Active",
+                insertby: "user"
+            },
+            type : 'get',
+            async : 'true',
+            dataType : 'json',
+            beforeSend: function() {
+        	   $.mobile.loading('show');
+            },
+            complete: function() {
+                $.mobile.loading('hide');
+            },
+            success : function(response) {
+                if(response.text == "0") {
+                    alert("error adding baby");
+                } else {
+                    alert("child added"+ response);
+                  //  $.mobile.changePage("#page", { transition: 'slide'});
+                }
+            },
+            error : function(request, error) {
+                alert(request.statusText + "\n" + request.responseText);
+            }
+        });
+  //  }
+}
+
+
+
+function addService() {  
+        alert(config.endpointsURL.InsertService);
+        $.ajax({
+            url : config.endpointsURL.InsertService,
+            data : {
+                servicename : $('#servicename').val(),
+                serviceforage : $('#serviceforage').val(),
+                description : $('#description').val(),
+                categoryid: "null",
+                status: "Active",
+                insertby: "user"
+            },
+            type : 'get',
+            async : 'true',
+            dataType : 'json',
+            beforeSend: function() {
+        	   $.mobile.loading('show');
+            },
+            complete: function() {
+                $.mobile.loading('hide');
+            },
+            success : function(response) {
+                if(response.text == "0") {
+                    alert("error adding service");
+                } else {
+                    alert("Service added"+ response);
+                  //  $.mobile.changePage("#page", { transition: 'slide'});
+                }
+            },
+            error : function(request, error) {
+                alert(request.statusText + "\n" + request.responseText);
+            }
+        });
+
+}
+
+
+
+
+function addOrg() {  
+        alert(config.endpointsURL.InsertOrganisation);
+        $.ajax({
+            url : config.endpointsURL.InsertOrganisation,
+            data : {
+                organisationname : $('#organisationname').val(),
+                address : $('#address').val(),
+                contactno : $('#contactno').val(),
+                email : $('#email').val(),
+                status: "Active",
+                insertby: "user"
+            },
+            type : 'get',
+            async : 'true',
+            dataType : 'json',
+            beforeSend: function() {
+        	   $.mobile.loading('show');
+            },
+            complete: function() {
+                $.mobile.loading('hide');
+            },
+            success : function(response) {
+                if(response.text == "0") {
+                    alert("error adding org");
+                } else {
+                    alert("added Org"+ response);
+                  //  $.mobile.changePage("#page", { transition: 'slide'});
+                }
+            },
+            error : function(request, error) {
+                alert(request.statusText + "\n" + request.responseText);
+            }
+        });
+
+}
+
+
+
+function addInvite() {  
+        alert(config.endpointsURL.InsertInvitation);
+        $.ajax({
+            url : config.endpointsURL.InsertInvitation,
+            data : {
+                email : $('#email2').val(),
+                relationshipwithchild : $('#relationshipwithchild').val(),
+                status: "Active",
+                insertby: "user1"
+            },
+            type : 'get',
+            async : 'true',
+            dataType : 'json',
+            beforeSend: function() {
+        	   $.mobile.loading('show');
+            },
+            complete: function() {
+                $.mobile.loading('hide');
+            },
+            success : function(response) {
+                if(response.text == "0") {
+                    alert("error adding org");
+                } else {
+                    alert("added Org"+ response);
+                  //  $.mobile.changePage("#page", { transition: 'slide'});
+                }
+            },
+            error : function(request, error) {
+                alert(request.statusText + "\n" + request.responseText);
+            }
+        });
+
+}
+
+
+
+
+
+$(document).on('pagebeforeshow', '#pagetwo,#pagethree,#pagefour,#pagefive,#pagesix,#pageseven,#pageeight', function(){
 	isLoggedIn();
 });
 
@@ -44,9 +232,9 @@ function isLoggedIn() {
 	var sessionId = sessionStorage.getItem("userId");
     
     if(sessionId == null || sessionId === "") {
-    	$.mobile.changePage("#pageone", { transition: 'slide', reverse: true });
+ //   	$.mobile.changePage("#pageone", { transition: 'slide'});
     } else {
-    	$.mobile.changePage("#pagetwo", { transition: 'slide'});
+    	$.mobile.changePage("#pagethree", { transition: 'slide'});
     }
 }
 
